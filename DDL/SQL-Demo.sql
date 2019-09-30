235a5fe785ff3137d300192e374b1db0bc53927d
Aemmerton <54901544+Aemmerton@users.noreply.github.com>
2019-09-30 10:48:36 -0600

    Update SQL-Demo.sql

--- DDL/SQL-Demo.sql
+++ DDL/SQL-Demo.sql
@@ -38,11 +38,23 @@ IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Customers
 -- To create a database table, we use the CREATE TABLE statement.
 -- Note that the order in which we create/drop tables is important
 -- because of how the tables are related via Foreign Keys.
+-- Note that square brackets around identifiers is a common standard in writing SQL.
+-- Databases in SQL group all their contents into something called a "schema". 
 CREATE TABLE Customers
 (
     -- The body of a CREATE TABLE will identify a comma-separated list of
     -- Column Declarations and Table Constraints.
-    CustomerNumber  int                 NOT NULL, -- NOT NULL means the data is required
+    CustomerNumber  int
+        -- The following is a PRIMARY KEY constraint that has a specific name
+        -- Primary Key constraints ensure a row of data being added to the table
+        -- will have to have a unique value for the Primary Key column(s)
+        CONSTRAINT PK_Customers_CustomerNumber
+            PRIMARY KEY
+        -- IDENTITY means the database will generate a unique whole-number
+        -- value for this column
+        IDENTITY(100, 1) -- The first number is the "seed",
+                         -- and the last number is the "increment"
+                                        NOT NULL,
     FirstName       varchar(50)         NOT NULL,
     LastName        varchar(60)         NOT NULL,
     [Address]       varchar(40)         NOT NULL,
@@ -54,8 +66,17 @@ CREATE TABLE Customers
 
 CREATE TABLE Orders
 (
-    OrderNumber     int                 NOT NULL,
-    CustomerNumber  int                 NOT NULL,
+    OrderNumber     int
+        CONSTRAINT PK_Orders_OrderNumber
+            PRIMARY KEY
+        IDENTITY(200, 1)                NOT NULL,
+    CustomerNumber  int
+        -- Foreign Key constraints ensure that when a row of data is being
+        -- inserted or updated, there is a row in the referenced table
+        -- that has the same value as its Primary Key
+        CONSTRAINT FK_Orders_CustomerNumber_Customers_CustomerNumber
+            FOREIGN KEY REFERENCES
+            Customers(CustomerNumber)   NOT NULL,
     [Date]          datetime            NOT NULL,
     Subtotal        money               NOT NULL,
     GST             money               NOT NULL,
@@ -64,6 +85,44 @@ CREATE TABLE Orders
 
 CREATE TABLE InventoryItems
 (
+    ItemNumber          varchar(5)
+        CONSTRAINT PK_InventoryItems_ItemNumber
+            PRIMARY KEY                     NOT NULL,
+    ItemDescription     varchar(50)             NULL,
+    CurrentSalePrice    money
+        CONSTRAINT CK_InventoryItems_CurrentSalePrice
+            CHECK (CurrentSalePrice > 0)    NOT NULL,
+    InStockCount        int                 NOT NULL,
+    ReorderLevel        int                 NOT NULL
+)
+
+CREATE TABLE OrderDetails
+(
+    OrderNumber     int
+        CONSTRAINT FK_OrderDetails_OrderNumber_Orders_OrderNumber
+            FOREIGN KEY REFERENCES
+            Orders(OrderNumber)         NOT NULL,
+    ItemNumber      varchar(5)
+        CONSTRAINT FK_OrderDetails_ItemNumber_InventoryItems_ItemNumber
+            FOREIGN KEY REFERENCES
+            InventoryItems(ItemNumber)  NOT NULL,
+    Quantity        int                 NOT NULL,
+    SellingPrice    money               NOT NULL,
+    Amount          AS Quantity * SellingPrice  ,
+    -- The following is a Table Constraint
+    -- A composite primary key MUST be done as a Table Constraint
+    -- because it involves two or more columns
+    CONSTRAINT PK_OrderDetails_OrderNumber_ItemNumber
+        PRIMARY KEY (OrderNumber, ItemNumber) -- Specify all the columns in the PK
+)
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'PaymentLogDetails')
+    DROP TABLE PaymentLogDetails
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Payments')
+    DROP TABLE Payments
+
+CREATE TABLE PaymentLogDetails
+(
     ItemNumber          varchar(5)          NOT NULL,
     ItemDescription     varchar(50)             NULL,
     CurrentSalePrice    money               NOT NULL,
@@ -71,7 +130,7 @@ CREATE TABLE InventoryItems
     ReorderLevel        int                 NOT NULL
 )
 
-CREATE TABLE OrderDetails
+CREATE TABLE Payments
 (
     OrderNumber     int                 NOT NULL,
     ItemNumber      varchar(5)          NOT NULL,
@@ -79,6 +138,4 @@ CREATE TABLE OrderDetails
     SellingPrice    money               NOT NULL,
     Amount          money               NOT NULL
 )
-
-
-/* ============================================================================================*/
+/*==========================================================*/
